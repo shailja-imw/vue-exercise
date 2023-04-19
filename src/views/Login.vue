6e211b8fc4f9b9bf5e74e91bf91e1d0989cc32ea
Shailja <shailja.goswami@improwised.com>
2023-04-19 11:40:03 +0530

    login and logout functionality added

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+const isLoggedIn = ref(false);
+
+</script>
+
+
+<template>
+    <div class="container mt-5">
+        <h4 class="mb-3">On button click You can login in and out</h4>
+        <div class="alert alert-success mb-2" role="alert">
+            <h4 class="alert-heading">{{ isLoggedIn ? "Well done!" : 'See You Soon'}}</h4>
+            <p>{{ isLoggedIn ? 'Hooray !! You are now our member' : "we're Sorry to See you Go" }}</p>
+        </div>
+        <button v-if="isLoggedIn" @click="isLoggedIn = false" class="btn btn-success">Logout</button>
+        <button v-else @click="isLoggedIn = true" class="btn btn-success">Login</button>
+    </div>
+</template>
