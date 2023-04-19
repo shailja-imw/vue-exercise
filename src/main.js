2754863498430a7252712f7189de853299c8b23f
Shailja <shailja.goswami@improwised.com>
2023-04-19 11:12:12 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
