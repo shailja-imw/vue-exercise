b73e2ffffb4f2acdd127cf15a75a7a41e2edd21e
Shailja <shailja.goswami@improwised.com>
2023-04-21 12:23:47 +0530

    header modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,13 @@
 <template>
-   <slot></slot>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+            <RouterLink class="nav-link" to='/active-users'>Active Users</RouterLink>
+        </li>
+        <li class="nav-item">
+            <RouterLink class="nav-link" to='/login' tabindex="-1" aria-disabled="true">Login</RouterLink>
+        </li>
+    </ul>
 </template>
